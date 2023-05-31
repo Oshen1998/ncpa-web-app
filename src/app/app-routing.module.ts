a1ba78f73bda751e1684df45f065b7e2edadb074
Sameera archchige <usjsameera@gmail.com>
2023-05-31 18:05:03 +0530

    style: :art: design login UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './Modules/auth/signin/signin.component';
+import { SignupComponent } from './Modules/auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
