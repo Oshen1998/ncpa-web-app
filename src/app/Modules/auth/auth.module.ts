cf7be30678ce62f8933d2fda1a7fa08ff001c37f
Sameera archchige <usjsameera@gmail.com>
2023-06-01 14:49:19 +0530

    style: :art: create sign in ui

--- src/app/Modules/auth/auth.module.ts
+++ src/app/Modules/auth/auth.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { CustomMaterialModule } from 'src/app/material.module';
 
 @NgModule({
-  declarations: [
-    SigninComponent,
-    SignupComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [SigninComponent, SignupComponent],
+  imports: [CommonModule, AuthRoutingModule, CustomMaterialModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './material.module';
+import { AuthModule } from './Modules/auth/auth.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
+    CustomMaterialModule,
+    AuthModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
