cddbc5b980df991f383e62fd96f9af4ec7e1be1b
Sameera archchige <usjsameera@gmail.com>
2023-06-02 18:03:15 +0530

    style: :art: create forgot and reset password ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,7 +21,7 @@ import {MatButtonModule} from '@angular/material/button';
     AuthModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
