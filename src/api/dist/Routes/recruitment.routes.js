8391d5aeb327846353ca15b482e480e5a35602c7
Oshen_Dikkumbura <oshen@echonlabs.com>
2023-06-14 09:59:15 +0530

    ccc

--- src/api/dist/Routes/recruitment.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RecruitmentRoutes = void 0;
-class RecruitmentRoutes {
-    route(app) {
-        //
-    }
-}
-exports.RecruitmentRoutes = RecruitmentRoutes;
--- src/api/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./Config/app");
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-app_1.default.listen(PORT, () => {
-    console.log('Server Connected Successfully.');
-    console.log('Server listening on port:' + PORT);
-});
